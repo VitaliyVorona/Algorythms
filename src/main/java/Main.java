ea6578f77123bb75df8f97104c91f38677e31692
vitaliyvorona <voronchik84@gmail.com>
2015-12-02 17:14:19 +0200

    pom added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,16 @@ public class Main {
     public static void main(String[] args) {
         //hello
         System.out.println("Hello world");
+        System.out.printf("The sum of sum is : %d", sum(4));
+
+    }
+
+    public static int sum(int x) {
+        int i = 1;
+        int sum = 0;
+        for (; i <= x; i++){
+            sum = sum + i;
+        }
+        return sum;
     }
 }
